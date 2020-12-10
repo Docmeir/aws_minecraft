dc2c1e6da3f6bf0c6817b191704cbc9965d988d5
Docmeir <33986799+Docmeir@users.noreply.github.com>
2020-12-10 00:35:49 -0500

    Update setup_aws_linux_2-minecraft_server.sh

--- setup_aws_linux_2-minecraft_server.sh
+++ setup_aws_linux_2-minecraft_server.sh
@@ -1,1 +1,12 @@
+#!bin/bash
+
+sudo yum install java-1.8.0-openjdk
+
+sudo mkdir MinecraftServer
+cd MinecraftServer
+aws s3 cp s3://minecraft-server-aws/setup/server.jar ./
+sudo java -Xms1G -jar server.jar nogui
+sudo rm eula.txt
+echo eula=true > eula.txt
+sudo java -Xms1G -jar server.jar nogui
 
